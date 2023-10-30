b89b77bfcbfce0d7325f2ecae2cce0ad8b026ace
ElRuso78 <daniandries78@gmail.com>
2023-10-30 13:41:24 +0100

    30/10

---
+++ Tema 5/T5E3.py
@@ -0,0 +1,4 @@
+contador = 0
+for i in range (1,201):
+    contador = contador+1
+    print (contador)
---
+++ Tema 5/T5E4.py
@@ -0,0 +1,4 @@
+numero=input()
+numero=int(numero)
+for i in range (1,numero + 1):
+    print (i)
