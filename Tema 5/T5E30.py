4f07b084edb7b7499e5527aaaa6277acb8f78177
ElRuso78 <daniandries78@gmail.com>
2023-10-31 13:36:51 +0100

    31/10

---
+++ Tema 5/T5E30.py
@@ -0,0 +1,9 @@
+maximo = int(input())
+
+for i in range (0,maximo+1):
+    for j in range (0,maximo-i):
+        print(" ", end=" ")
+    for k in range (1,2*i):
+        print("*", end=" ")
+    print("")
+    
