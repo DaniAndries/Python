90cc144d5afac361963a52f5a190726ed6134a59
ElRuso78 <daniandries78@gmail.com>
2023-11-05 20:22:41 +0100

    5/11

--- Tema 5/T5E34.py
+++ Tema 5/T5E34.py
@@ -2,7 +2,8 @@ numero = int(input())
 contador = 0
 while numero != 0:
     digito = int(numero % 10)
-    print(digito)
+    print(digito, end=" ")
     numero = int(numero / 10)
     contador = contador + 1
+print("")
 print("Hay", contador, "dígitos")
